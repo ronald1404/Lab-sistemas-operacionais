74b2798e6b2d4d0733c22b045e3b301edc9eb620
ronald <ronald.andrade.02@gmail.com>
2022-12-15 22:13:48 -0300

    escalonador

--- Lab05/Escalonador.h
+++ Lab05/Escalonador.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 typedef struct
 {
     int Prioridade;
@@ -53,4 +54,76 @@ void Retira(int posicao, Escalonador *escalonador, Processo *Item)
     escalonador->Ultimo--;
     for (Aux = posicao; Aux < escalonador->Ultimo; Aux++)
         escalonador->processos[Aux] = escalonador->processos[Aux + 1];
+}
+
+void BubbleSort(Processo arr[], int n) {
+    int i,j;
+    Processo aux;
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j].Prioridade > arr[j+1].Prioridade){
+                aux = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = aux;
+            }
+        }
+    }
+}
+
+void round_robin(Escalonador escalonador,float time_slice){
+    Processo p;
+
+    p.Prioridade = 4;
+    p.Tempo = 200;
+    Insere(p, &escalonador);
+    p.Prioridade = 1;
+    p.Tempo = 300;
+    Insere(p, &escalonador);
+    p.Prioridade = 2;
+    p.Tempo = 25;
+    Insere(p, &escalonador);
+
+
+    while(!Vazia(escalonador)){
+        for (int i = escalonador.Primeiro; i < escalonador.Ultimo; i++){
+            if ((escalonador.processos[i].Tempo) - time_slice <= 0){
+            printf("P%d, %0.2f*\n",escalonador.processos[i].id, escalonador.processos[i].Tempo );
+            Retira(i,&escalonador,&p);
+            }
+            else{
+            printf("P%d, %0.2f\n",escalonador.processos[i].id, escalonador.processos[i].Tempo);
+            escalonador.processos[i].Tempo -= time_slice;
+            }
+        }
+    }
+}
+
+void prioridade(Escalonador escalonador,float time_slice){
+    Processo p;
+
+    p.Prioridade = 4;
+    p.Tempo = 200;
+    Insere(p, &escalonador);
+    p.Prioridade = 1;
+    p.Tempo = 300;
+    Insere(p, &escalonador);
+    p.Prioridade = 2;
+    p.Tempo = 25;
+    Insere(p, &escalonador);
+
+    //Ordenar por prioridade
+    BubbleSort(escalonador.processos,3);
+    while(!Vazia(escalonador)){
+        for (int i = escalonador.Primeiro; i < escalonador.Ultimo; i++){
+            if ((escalonador.processos[i].Tempo) - time_slice <= 0){
+            printf("P%d, %0.2f*\n",escalonador.processos[i].id, escalonador.processos[i].Tempo );
+            Retira(i,&escalonador,&p);
+            }
+            else{
+            printf("P%d, %0.2f\n",escalonador.processos[i].id, escalonador.processos[i].Tempo);
+            escalonador.processos[i].Tempo -= time_slice;
+            }
+        }
+    }
+
 }
---
+++ Lab05/prioridade.c
@@ -0,0 +1,18 @@
+// #include "Circular.h"
+#include "Escalonador.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    Escalonador escalonador;
+    FLVazia(&escalonador);
+
+    prioridade(escalonador,10);
+
+
+
+    return 0;
+}
